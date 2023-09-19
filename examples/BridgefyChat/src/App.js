fb48fa4e3eb63c2c0bedf520968064c2ec694b1c
Miguel Tec <82394801+mike-bridgefy@users.noreply.github.com>
2023-09-19 16:54:30 -0600

    chore: create index.js and move App.js

--- examples/BridgefyChat/src/App.js
+++ examples/BridgefyChat/src/App.js
@@ -1,7 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 
-import AppNavigator from './src/navigation/AppNavigator';
+import AppNavigator from './navigation/AppNavigator';
 
 export default function App() {
   return (
