9d1453ed979e4cf82d21da7aadfd7160c2266b0f
Miguel Tec <82394801+mike-bridgefy@users.noreply.github.com>
2024-07-02 13:58:31 -0600

    feat: add common_button and message_field_box components

---
+++ example/src/presentation/components/common_button.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Button } from 'react-native-paper';
+import { globalColors } from '../theme/global.styles';
+
+interface Props {
+  label: string;
+  icon: string;
+  disabled?: boolean;
+  onPress: () => void;
+}
+
+export const CommonButton = ({
+  label,
+  icon,
+  disabled = false,
+  onPress,
+}:Props) => {
+  return (
+    <Button
+      icon={icon}
+      mode='elevated'
+      disabled={disabled}
+      textColor={globalColors.primaryColor}
+      buttonColor="white"
+      onPress={()=>onPress()}
+    >
+      {label}
+    </Button>
+  )
+}
