7aa7c463b079976f530f6daf2477795b89edf209
Miguel Tec <82394801+mike-bridgefy@users.noreply.github.com>
2024-07-02 10:37:48 -0600

    chore: add global styles

---
+++ example/src/presentation/components/IonIcon.tsx
@@ -0,0 +1,13 @@
+import Icon from 'react-native-vector-icons/Ionicons';
+import { globalColors } from '../theme/global.styles';
+
+interface Props {
+  name: string;
+  color?: string;
+  size?: number;
+}
+export const IonIcon = ({name, color=globalColors.primaryColor, size=25}: Props) => {
+  return (
+    <Icon name={ name } size={ size } color={ color } />
+  )
+}
