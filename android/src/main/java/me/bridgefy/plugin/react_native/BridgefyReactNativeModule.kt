0f4976fe10802d41333ec8fae62b87837409957f
Julian de la Orta <julian@bridgefy.me>
2024-11-01 13:58:50 -0600

    fix: update bridgefy SDK classes

--- android/src/main/java/me/bridgefy/plugin/react_native/BridgefyReactNativeModule.kt
+++ android/src/main/java/me/bridgefy/plugin/react_native/BridgefyReactNativeModule.kt
@@ -12,7 +12,7 @@ import com.facebook.react.bridge.WritableMap
 import com.facebook.react.modules.core.DeviceEventManagerModule
 import java.util.UUID
 import me.bridgefy.Bridgefy
-import me.bridgefy.logger.enum.LogType
+import me.bridgefy.logger.enums.LogType
 import me.bridgefy.commons.TransmissionMode
 import me.bridgefy.commons.exception.BridgefyException
 import me.bridgefy.commons.listener.BridgefyDelegate
