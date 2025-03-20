cf4225767b2b54da633bd86702d08b13e85e1135
Miguel Tec <82394801+mike-bridgefy@users.noreply.github.com>
2025-03-20 11:49:00 -0600

    chore: fix lint warnings

--- example/App.tsx
+++ example/App.tsx
@@ -2,13 +2,15 @@
 import {PaperProvider} from 'react-native-paper';
 import {NavigationContainer} from '@react-navigation/native';
 import {HomeScreen} from './src/presentation/screens';
-import { IonIcon } from './src/presentation/components/IonIcon';
+import {IonIcon} from './src/presentation/components/IonIcon';
+
+const IconComponent = (props: any) => <IonIcon {...props} />;
 
 export const App = () => {
   return (
     <PaperProvider
       settings={{
-        icon: props => <IonIcon {...props} />,
+        icon: IconComponent,
       }}>
       <NavigationContainer>
         <HomeScreen />
--- example/src/presentation/components/IonIcon.tsx
+++ example/src/presentation/components/IonIcon.tsx
@@ -1,5 +1,5 @@
 import {globalColors} from '../theme/global.styles';
-import Icon from 'react-native-vector-icons/Ionicons'
+import Icon from 'react-native-vector-icons/Ionicons';
 
 interface Props {
   name: string;
